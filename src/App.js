a5bf840c5a6104b04e5e57c63c390780eca12dd8
Malivuca <miguelvargasdemarque@gmail.com>
2019-12-21 17:48:06 -0300

    Added header and links to other pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 
 import shopPage from './pages/shop/shop.component.jsx';
 
+import Header from './components/header/header.component.jsx';
+
 /*const HatsPage = () => (
   <div>
     <h1> Hats Page </h1>
@@ -18,6 +20,7 @@ import shopPage from './pages/shop/shop.component.jsx';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route exact={true} path='/shop' component={shopPage} />
