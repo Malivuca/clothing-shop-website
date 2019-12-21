455cd2e21be1b05c71ba460e94698d0e69a8adf1
Malivuca <miguelvargasdemarque@gmail.com>
2019-12-21 03:11:09 -0300

    New shop page and collection preview

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
-const HatsPage = () => (
+import shopPage from './pages/shop/shop.component.jsx';
+
+/*const HatsPage = () => (
   <div>
     <h1> Hats Page </h1>
   </div>
 );
+*/
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route exact={true} path='/shop' component={shopPage} />
       </Switch>
     </div>
   );
